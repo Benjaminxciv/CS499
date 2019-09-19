87e9f31ee21d540701695d800e37da2e5e667db2
Adam Whisnant <asqapro11@gmail.com>
2019-09-18 22:42:41 -0500

    Added test to confirm grid changes worked.

--- sources/simulation.cpp
+++ sources/simulation.cpp
@@ -133,10 +133,13 @@ int main()
     grid& sim_grid = grid::get_instance(10,10);
     boulder* bould = new boulder(1,1);
     sim_grid.set_cell_contents(1, 1, bould);
-    boulder* bould2 = nullptr;
-    //sim_grid.get_cell_contents(1,1,&bould2);
-    point* p = bould2->get_loc();
-    std::cout << p->x_loc << std::endl << p->y_loc << std::endl;
+    environment_object* empty_obj = sim_grid.get_cell_contents(1,1);
+	if(empty_obj->get_type() == "boulder")
+	{
+		std::cout << "Got a boulder!" << endl;
+	}
+    //point* p = bould2->get_loc();
+    //std::cout << p->x_loc << std::endl << p->y_loc << std::endl;
     std::cin.get();
     return 0;
 }
