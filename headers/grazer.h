933b8b94523278e4d48545956005cad5e2c1cb15
Adam Whisnant <asqapro11@gmail.com>
2019-09-27 10:55:03 -0500

    Changed DMCO -> grazer

--- headers/grazer.h
+++ headers/grazer.h
@@ -1,15 +1,15 @@
-#ifndef DMCO_H
-#define DMCO_H
+#ifndef GRAZER_H
+#define GRAZER_H
 
 #include <string>
 #include "environment_object.h"
 
-class dmco : public environment_object
+class grazer : public environment_object
 {
     private:
     public:
-        dmco(int, int);
-        ~dmco();
+        grazer(int, int);
+        ~grazer();
         std::string get_type();
 };
 
