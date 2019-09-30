b78b12dfbdce4affe90690aa9fec0ca75f8b68b2
Adam Whisnant <asqapro11@gmail.com>
2019-09-29 20:29:25 -0500

    Added comments.

--- sources/environment_object.cpp
+++ sources/environment_object.cpp
@@ -13,11 +13,26 @@ environment_object::~environment_object()
     delete this->location;
 }
 
+/*Name: get_loc
+Purpose: Get the X and Y location of the object
+Parameters: 
+    NA
+Returns: Pointer to a point structure containing X and Y location*/
 point* environment_object::get_loc()
 {
     return this->location;
 }
 
+/*Name: set_location
+Purpose: Set the X and Y location of the objectz
+Parameters: 
+    x_loc: int
+        X location of cell to access
+    y_loc: int
+        Y location of cell to access
+    environ_obj: environment_object*
+        Resident to place in the cell
+Returns: NA*/
 void environment_object::set_location(int set_x_loc, int set_y_loc)
 {
     this->location->x_loc = set_x_loc;
--- sources/grid.cpp
+++ sources/grid.cpp
@@ -22,36 +22,45 @@ grid::~grid()
     delete [] this->cells;
 }
 
+/*Name: get_instance
+Purpose: Get reference to singleton grid object
+Parameters: 
+    x_size: int
+        Defines width of grid
+    y_size: int
+        Defines height of grid
+Returns: Reference to singleton grid object*/
 grid& grid::get_instance(int x_size, int y_size)
 {
     static grid grid_instance(x_size, y_size);
     return grid_instance;
 }
 
-//Name: grid::get_cell_contents
-//Purpose: Get resident of some cell at X and Y location
-//Parameters: 
-    //x_loc: int
-        //X location of cell to access
-    //y_loc: int
-        //Y location of cell to access
-//Last edit: 09/18/19
+
+/*Name: get_cell_contents
+Purpose: Get pointer to the resident of some cell at X and Y location
+Parameters: 
+    x_loc: int
+        X location of cell to access
+    y_loc: int
+        Y location of cell to access
+Returns: Pointer to resident of cell*/
 environment_object* grid::get_cell_contents(int x_loc, int y_loc)
 {
     grid_cell* cell = &(this->cells[x_loc*this->height+y_loc]);
     return cell->environ_obj;
 }
 
-//Name: grid::set_cell_contents
-//Purpose: Set resident of some cell at X and Y location
-//Parameters: 
-    //x_loc: int
-        //X location of cell to access
-    //y_loc: int
-        //Y location of cell to access
-    //environ_obj: environment_object*
-        //Pointer to environment_object that will be put in the cell
-//Last edit: 09/18/19
+/*Name: set_cell_contents
+Purpose: Set the resident of some cell at X and Y location
+Parameters: 
+    x_loc: int
+        X location of cell to access
+    y_loc: int
+        Y location of cell to access
+    environ_obj: environment_object*
+        Resident to place in the cell
+Returns: NA*/
 void grid::set_cell_contents(int x_loc, int y_loc, environment_object* environ_obj)
 {
     grid_cell* cell = &(this->cells[x_loc*this->height+y_loc]);
