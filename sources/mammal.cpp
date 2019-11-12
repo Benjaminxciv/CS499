8cef41eb7e7a6d5f45bceb93f175a8186a92e88f
Adam Whisnant <asqapro11@gmail.com>
2019-11-11 20:23:58 -0600

    Fixed pointer to non-pointer in mammal

--- sources/mammal.cpp
+++ sources/mammal.cpp
@@ -35,32 +35,32 @@ void mammal::gain_energy(int energy)
 
 void mammal::move(direction dir, int speed)
 {
-    point* curr_loc = this->get_loc();
+    point curr_loc = this->get_loc();
     switch(dir)
     {
         case up:
-            this->set_location(curr_loc->x_loc+1, curr_loc->y_loc);
+            this->set_location(curr_loc.x_loc+1, curr_loc.y_loc);
             break;
         case up_right:
-            this->set_location(curr_loc->x_loc+1, curr_loc->y_loc);
+            this->set_location(curr_loc.x_loc+1, curr_loc.y_loc);
             break;
         case right:
-            this->set_location(curr_loc->x_loc+1, curr_loc->y_loc);
+            this->set_location(curr_loc.x_loc+1, curr_loc.y_loc);
             break;
         case down_right:
-            this->set_location(curr_loc->x_loc+1, curr_loc->y_loc);
+            this->set_location(curr_loc.x_loc+1, curr_loc.y_loc);
             break;
         case down:
-            this->set_location(curr_loc->x_loc+1, curr_loc->y_loc);
+            this->set_location(curr_loc.x_loc+1, curr_loc.y_loc);
             break;
         case down_left:
-            this->set_location(curr_loc->x_loc+1, curr_loc->y_loc);
+            this->set_location(curr_loc.x_loc+1, curr_loc.y_loc);
             break;
         case left:
-            this->set_location(curr_loc->x_loc+1, curr_loc->y_loc);
+            this->set_location(curr_loc.x_loc+1, curr_loc.y_loc);
             break;
         case up_left:
-            this->set_location(curr_loc->x_loc+1, curr_loc->y_loc);
+            this->set_location(curr_loc.x_loc+1, curr_loc.y_loc);
             break;
     }
     this->energy -= this->e_output * (speed / 5);
