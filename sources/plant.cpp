11cda94ef5b97729e62af14116177cb54ebaab0b
MdgAki <mdgilstrap@gmail.com>
2019-11-12 21:07:59 -0600

    small changes so master can be rebased

--- sources/plant.cpp
+++ sources/plant.cpp
@@ -1,7 +1,7 @@
 /*
 Name: plant.cpp
 Purpose: Class defining all the plant's function, such as location and growing pattern.
-Last edit: 11-10-2019
+Last edit: 11-12-2019
 Last editor: MG
 */
 
@@ -63,9 +63,6 @@ void plant::set_seed_pod_values()
     int MAX_SEED_CAST_DISTANCE = info_gather ->getMaxSeedCastDistance() ;
     int MAX_SEED_NUMBER = info_gather->getMaxSeedNumber();
     double SEED_VIABILITY = info_gather->getSeedViability();
-    
-
-    srand(time(0));
 
     int seed_pod_distance = rand() %  MAX_SEED_CAST_DISTANCE + 0;
     int seed_pod_seed_number = (rand() %  MAX_SEED_NUMBER + 0) * SEED_VIABILITY; 
@@ -88,8 +85,8 @@ void plant::radially_disperse_seed()
         theta = theta + rand() % (360/seed_pod_seed_number) + 0;
         
         sim_message& message = sim_message::get_instance();
+        message.look_at_cell()
         message.request_action("occupied", target_center_x, target_center_y);
-        message.process_message();
         // check if cell is empty
         // need to check in to see if this would be correct way to test logic. 
         //if (message.request_action("occupied", target_center_x, target_center_y == false)
@@ -103,14 +100,12 @@ void plant::radially_disperse_seed()
 
 void plant::act()
 {
-    //if plant fully grown
+    //TODO:: add if check for plant fully grown
     sim_message& message = sim_message::get_instance();
     message.get_current_time();
-    message.process_message();
     time_container current_time = message.get_time_info();
 
     message.get_future_time(0, 0, 1);
-    message.process_message();
     time_container future_time = message.get_time_info();
     
     if (current_time.time_hour == future_time.time_hour)
